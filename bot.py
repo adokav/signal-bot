b3059bf1e2d6477e36e919229c14b49648ee3818
adokav <ademmkavlak@gmail.com>
2026-04-24 22:34:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,21 @@ import os
 import time
 import requests
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "8519535855:AAFF2bcBsDsYGb4gLuONP_T29xuQn7PbQPU"
+CHAT_ID = "6393685498"
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": text}
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
     requests.post(url, data=data)
 
-def get_btc_price():
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    r = requests.get(url)
-    return r.json()["price"]
+send_message("BOT BAŞLADI 🚀")
 
-print("BOT BAŞLADI")
+while True:
+    pass
 
 send_message("BOT BAŞLADI 🚀")
 
