2f9289b338afcd9bdf73223563dc1658b0e2efaf
adokav <ademmkavlak@gmail.com>
2026-08-20 13:27:42 +0300

    Expose memecoin radar contracts

---
+++ memecoin_radar/__init__.py
@@ -0,0 +1,21 @@
+"""Solana memecoin early-opportunity research module."""
+
+from .contracts import (
+    ExecutionMode,
+    ExecutionPolicy,
+    GateEvidence,
+    GateState,
+    HARD_GATES,
+    MemecoinAssessment,
+    Qualification,
+)
+
+__all__ = [
+    "ExecutionMode",
+    "ExecutionPolicy",
+    "GateEvidence",
+    "GateState",
+    "HARD_GATES",
+    "MemecoinAssessment",
+    "Qualification",
+]
